879ea3bcb63bf1ba82187eab32e77047b1982486
Kevin Do <kevind0501@gmail.com>
2021-11-02 13:23:25 -0700

    added JS to main page html

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// import functions and grab DOM elements
+import { setUser, generateUser } from './utils.js'
+// import functions and grab DOM element
+
+const userForm = document.getElementById('user-form');
 
 // initialize global state
 
@@ -6,3 +9,13 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(userForm);
+    const user = generateUser(formData);
+
+    setUser(user);
+    window.location.replace('../opener');
+});
