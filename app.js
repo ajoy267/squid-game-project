2503076e9dabb75e50c0c6331d31c472c0d28030
ajoy267 <andrew.joy12@hotmail.com>
2021-11-04 14:48:17 -0700

    remopved uncessary code

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-import { setUser, generateUser } from './utils.js'
-// import functions and grab DOM element
+import { setUser, generateUser } from './utils.js';
 
 const userForm = document.getElementById('user-form');
-
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 
 userForm.addEventListener('submit', (e) => {
     e.preventDefault();
